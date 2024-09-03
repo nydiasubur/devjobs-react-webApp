35c2501b4560cbdfbdaff6002950d2260dbbceca
nydia-subur <nydia.subur.2016@sis.smu.edu.sg>
2024-09-03 23:58:36 +0700

    netlify debug 2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // Ensure this matches your Netlify settings
+    outDir: "dist",
   },
+  assetsDir: "assets", // Make sure assets are included in the build
 });
