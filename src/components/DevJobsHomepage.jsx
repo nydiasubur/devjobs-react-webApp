a37eb774150f6a92a8c8739e05eeafe42a418e14
nydia-subur <nydia.subur.2016@sis.smu.edu.sg>
2024-09-04 22:18:21 +0700

    added the load more function to devjobshomepage!

--- src/components/DevJobsHomepage.jsx
+++ src/components/DevJobsHomepage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SearchBar from "./SearchBar";
 import data from "../data.json";
 import { useState } from "react";
@@ -7,11 +7,24 @@ import ExpandedJobDescription from "./ExpandedJobDescription";
 import TopNavBar from "./TopNavBar";
 import Footer from "./Footer";
 export default function DevJobsHomepage() {
-  const originalJobList = data;
-  const [jobList, setJobList] = useState(data);
+  const originalJobList = data; // original data not to be changed
+  const [jobList, setJobList] = useState(data); //job lists that gets updated from filtering
   const [selectedJob, setSelectedJob] = useState(null);
   const [isJobSelected, setIsJobSelected] = useState(false);
+  const [page, setPage] = useState(1);
+  const jobCardPerPage = 6;
+  const [temporaryJobListForLoadMore, setTemporaryJobListForLoadMore] =
+    useState([]);
 
+  // Effect to update temporary job list when page or jobList changes
+  useEffect(() => {
+    setTemporaryJobListForLoadMore(jobList.slice(0, page * jobCardPerPage));
+  }, [page, jobList]);
+
+  // Reset page to 1 when jobList changes due to filtering
+  useEffect(() => {
+    setPage(1);
+  }, [jobList]);
   return (
     <div>
       <TopNavBar setIsJobSelected={setIsJobSelected} />
@@ -32,15 +45,20 @@ export default function DevJobsHomepage() {
           <div className=" container listOfCards mt-5">
             <div className="row gx-5 gy-5 d-flex justify-content-between">
               <ListOfJobCards
-                jobList={jobList}
+                jobList={temporaryJobListForLoadMore}
                 selectedJob={selectedJob}
                 setSelectedJob={setSelectedJob}
                 setIsJobSelected={setIsJobSelected}
               />
             </div>
-            <button className="btn load-more-button main-button-style ">
-              Load More
-            </button>
+            {temporaryJobListForLoadMore.length < jobList.length && (
+              <button
+                className="btn load-more-button main-button-style"
+                onClick={() => setPage(page + 1)}
+              >
+                Load More
+              </button>
+            )}
           </div>
         </div>
       )}
